9fa3c46b95b35b863c275a86f774d6809c40acec
Cristhian Cano <criscano_1994@hotmail.com>
2022-06-06 20:10:05 -0500

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,5 +3,24 @@ def index
     @portfolio_items = Portfolio.all    
 end
 
+def new
+    @portfolio_items = Portfolio.new
+    
+end
+
+def create
+    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle,:body))
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to portfolios_path, notice: "Your Portfolio Item is now live." }
+        
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        
+      end
+    end
+    
+end
 
 end
