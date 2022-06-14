27d2836887628ee92920c743bdb1d93269b853ea
Cristhian Cano <criscano_1994@hotmail.com>
2022-06-14 08:46:16 -0500

    Install petergate gem for authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,19 @@ include DeviseWhitelist
 include SetSource
 
 include DefaultPageContent
+before_action :set_copyright
 
-    
+def set_copyright
+    @copyright = DevcampViewTool::Renderer.copyright 'Cristhian Cano','All rights reserved'
+end
+
+end
+
+module DevcampViewTool
+    class Renderer
+        def self.copyright name, msg
+            "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+        end
+    end
+
 end
